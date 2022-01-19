074ae14c4399858dcb1128f2c4d8430c821df570
DesySimeonova <96577243+DesySimeonova@users.noreply.github.com>
2022-01-19 21:10:26 +0200

    Update hangman_project.cpp

--- Hangman_project/hangman_project.cpp
+++ Hangman_project/hangman_project.cpp
@@ -1,6 +1,6 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <string>
+#include <vector>
 #include <windows.h>
 #include <stdlib.h>
 #include <ctime>
@@ -17,30 +17,29 @@ void Menu(unsigned& chosenOption)
 	if (chosenOption < 1 || chosenOption>3)
 	{
 		cout << "Invalid input! Try again: ";
-		cin >> chosenOption;//taka vqrno li e?
+		cin >> chosenOption;
 	}
 	SetConsoleTextAttribute(h, 7);
 }
-
 
 void Settings(unsigned& wordLength, unsigned& numberAttempts)
 {
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(h, 10);
 	cout << endl;
-	cout << "1. Choose the word's length:" << endl << " 1. 3-5 letters" << endl << " 2. 5-7 letters" << endl << " 3. 7-10 letters"<< endl << " 4. Skip this option.";
-	cout<<endl<< "Enter a number: ";
+	cout << "1. Choose the word's length:" << endl << " 1. 3-5 letters" << endl << " 2. 6-7 letters" << endl << " 3. 8-10 letters" << endl << " 4. Skip this option.";
+	cout << endl << "Enter a number: ";
 	cin >> wordLength;
 	if (wordLength < 1 || wordLength > 4) //napravi dopulnitelna proverka dali e chislo!!!
 	{
 		cout << "Invalid input! Try again: ";
-		cin >> wordLength;//taka vqrno li e? 
+		cin >> wordLength;
 	}
 
 	cout << endl;
 	unsigned chosenAttemptsOption;
 	SetConsoleTextAttribute(h, 11);
-	cout<< "2. Choose the number of the attempts:"<<endl<<" 1. Choose a number between 1 and 20. "<<endl<<" 2. Skip this option."<<endl;
+	cout << "2. Choose the number of the attempts:" << endl << " 1. Choose a number between 1 and 20. " << endl << " 2. Skip this option." << endl;
 	cout << "Enter a number: ";
 	cin >> chosenAttemptsOption;
 	if (chosenAttemptsOption == 1)
@@ -50,7 +49,7 @@ void Settings(unsigned& wordLength, unsigned& numberAttempts)
 		if (numberAttempts < 1 || numberAttempts > 20)
 		{
 			cout << "Invalid input! Try again: ";
-			cin >> numberAttempts;//taka vqrno li e?
+			cin >> numberAttempts;
 		}
 	}
 	else if (chosenAttemptsOption == 2)
@@ -60,34 +59,55 @@ void Settings(unsigned& wordLength, unsigned& numberAttempts)
 		cout << "Invalid input! Try again: ";
 		cin >> numberAttempts;
 	}
-	 
+
 	SetConsoleTextAttribute(h, 7);
 }
 
-void getRandomWord(const string* dictionary, string& gameword, unsigned wordLength) //how to get a random number rigth???
+void getRandomWord(const vector<vector<string>> dictionary, string& gameword, unsigned wordLength)
 {
 	unsigned randomWordIndex = 0;
-	srand(time(0));//how does it work?
+	unsigned rowNumber = 0;
+	srand(time(0)); //how does it work?
 	if (wordLength == 1)
-		randomWordIndex = rand() % 8;
-	else if(wordLength==2)
-		randomWordIndex = rand() % 14+6;
-	else if(wordLength==3)
-		randomWordIndex = rand() % 12+7;
+		randomWordIndex = rand() % dictionary[0].size();
+	else if (wordLength == 2)
+	{
+		randomWordIndex = rand() % dictionary[1].size();
+		rowNumber = 1;
+	}
+	else if (wordLength == 3)
+	{
+		randomWordIndex = rand() % dictionary[2].size();
+		rowNumber = 2;
+	}
 	else
-		randomWordIndex = rand() % 19;
-	gameword = dictionary[randomWordIndex];
+	{
+		rowNumber = rand() % dictionary.size();
+		randomWordIndex = rand() % dictionary[rowNumber].size();
+
+	}
+
+	gameword = dictionary[rowNumber][randomWordIndex];
 }
 
 void hideWord(const string gameWord, char* hiddenWord)
 {
 	unsigned gameWordLength = gameWord.length();
 	for (unsigned i = 0; i < gameWordLength; i++)
-		hiddenWord[i] = '*';
+		hiddenWord[i] = '_';
 	hiddenWord[gameWordLength] = '\0';
 }
 
-bool doesItContain(const string gameWord, const char guess,char* hiddenWord )
+void printHiddenWord(char* hiddenWord)
+{
+	while (*hiddenWord != '\0')
+	{
+		cout << *hiddenWord << " ";
+		hiddenWord++;
+	}
+}
+
+bool doesItContain(const string gameWord, const char guess, char* hiddenWord)
 {
 	unsigned gameWordLength = gameWord.length();
 	bool doesItContain = false;
@@ -103,7 +123,7 @@ bool doesItContain(const string gameWord, const char guess,char* hiddenWord )
 		return true;
 	else
 		return false;
-	
+
 }
 
 bool guessCheck(char guess)
@@ -123,31 +143,32 @@ bool areTheSame(string gameWord, unsigned gameWordLength, char* hiddenWord)
 	return true;
 }
 
-void hangman (const string* dictionary, unsigned wordLength, unsigned numberAttempts, unsigned& chosenOption)
+void hangman(const vector<vector<string>> dictionary, unsigned wordLength, unsigned numberAttempts, unsigned& chosenOption)
 {
-	while(chosenOption!=1)
-	{ 
+	while (chosenOption != 1)
+	{
 		Menu(chosenOption);
 		if (chosenOption == 3)
-			exit(-1);//how to close the program???
+			exit(-1);
 		else if (chosenOption == 2)
-		{      
-		    Settings(wordLength, numberAttempts);
+		{
+			Settings(wordLength, numberAttempts);
 		}
 
 		cout << endl;
 	}
-	cout << "Let's start the game!"<<endl;
-	string gameWord= " ";
+	cout << "Let's start the game!" << endl;
+	string gameWord = " ";
 	char hiddenWord[MAX_WORD_LENGTH];
 	getRandomWord(dictionary, gameWord, wordLength);
-	//cout << gameword;
 	hideWord(gameWord, hiddenWord);
 	char guess;
 	bool win = true;
-	while(numberAttempts!=0)
-	{ 
-		cout << "Attempts left: " << numberAttempts << ". Guess the word " << hiddenWord <<endl;
+	while (numberAttempts != 0)
+	{
+		cout << "Attempts left: " << numberAttempts << ". Guess the word ";
+		printHiddenWord(hiddenWord);
+		cout << endl;
 		cout << "Enter your guess: ";
 		cin >> guess;
 		if (!guessCheck(guess))
@@ -166,55 +187,55 @@ void hangman (const string* dictionary, unsigned wordLength, unsigned numberAtte
 			if (gameWord[i] != hiddenWord[i])
 				win = false;
 		}
-		
+
 		if (win)
 		{
-			cout <<endl<< "Congrats! You guessed the word!"<<endl;
+			cout << endl << "Congrats! You guessed the word " << gameWord << "!" << endl;
 			break;
 		}
 
-    }
+	}
 	if (!win)
 	{
 		cout << "Sorry, you lost the game! The word is " << gameWord << "." << endl;
 	}
-	
-	
+
+	cout << endl;
 	Menu(chosenOption);
 }
 
 int main()
 {
-	string test = "cat";
-	//cout << boolalpha << doesItContain(test, 't')<<endl;
 
-	string dictionary[21] = { "bed", "cat", "dog", "card", "park", "rain", "zombi", "lemon", "scare", "coffee",
-						   "camera", "castle", "success", "tonight", "desktop", "building", "engineer", "location", "education", "chocolate",
-						   "motivation" };
+	vector <vector<string>> dictionary = {
+	{ "bed", "cat", "dog", "card", "park", "rain", "zombi", "lemon", "scare" },
+	{ "coffee", "camera", "castle", "success", "tonight", "desktop" },
+	{"building", "engineer", "location", "education", "chocolate", "motivation" }
+	};
+
 	unsigned wordLength = 0;
 	unsigned numberAttempts = 5;
 	unsigned chosenOption = 0;
 	while (chosenOption != 3)
 	{
-		switch(chosenOption)
-		{ 
+		switch (chosenOption)
+		{
 		case 0:
 			hangman(dictionary, wordLength, numberAttempts, chosenOption);
 			break;
 		case 1:
 			hangman(dictionary, wordLength, numberAttempts, chosenOption);
 			break;
-		case 2: 
+		case 2:
 			Settings(wordLength, numberAttempts);
 			chosenOption = 0;
 			break;
 		}
 	}
 	exit(-1);
-	
-	
 
 
 	return 0;
 }
+
 
